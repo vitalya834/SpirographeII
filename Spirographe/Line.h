425fbf116c134b664e4d4f73f0835baec44f3d82
vitalya834 <vitalya_belykn@mail.ru>
2019-03-21 13:03:03 +0700

    Добавьте файлы проекта.

---
+++ Spirographe/Line.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace sf ;
+using namespace std ;
+
+class Line : public VertexArray, public Transformable
+{
+public:
+	
+private:
+};
+
